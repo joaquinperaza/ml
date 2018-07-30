5c0ff729e4e1d3db297fc774cb2bc7086ae53ba1
joaquinperaza <joaquin@peraza.uy>
2018-07-30 20:36:52 -0300

    cambio v2

--- App/ml.js
+++ App/ml.js
@@ -1,30 +1,48 @@
-var articulos_lista = JSON.parse(localStorage.getItem('articulos'));
 
-if (articulos_lista==null) {
-	articulos_lista=[];
-	localStorage.setItem("articulos",JSON.stringify(articulos_lista));
-}
 
-$(document).ready(function(){
-	$("#save").click(function(){
-		guardarArticulo();
-	});
-});
+ var articulos_lista = JSON.parse(localStorage.getItem('articulos'));
+ if (articulos_lista == null){
+  articulos_lista=[];
+  localStorage.setItem("articulos", JSON.stringify(articulos_lista));
+ }
 
-function guardarArticulo(){
-	var title = $("#title").val();
-	var imageURL = $("#url").val();
-	var precio = $("#precio").val();
-	var description = $("#description").val();
+ $(document).ready(function(){
+ $("#save").click(function(){
+  GuardarArt();
+ });
 
-	var articulo = {
-		title: title,
-		imageURL: imageURL,
-		precio: precio,
-		description: description
-	}
-	console.log(articulo);
-	articulos_lista.push(articulo);
-console.log(articulos_lista);
-	localStorage.setItem("articulos", JSON.stringify(articulos_lista));
-}
+ });
+
+ function GuardarArt(){
+
+  var titulo = $("#title").val();
+  var URL = $("#URL").val();
+ var Precio = $("#price").val();
+  var Descrip = $("#description").val();
+
+  var articulo = {
+      Nombre: title,
+      Imagen: URL,
+      PrecioArt: price,
+      Dec: description,
+       }
+
+       articulos_lista.push(articulo);
+
+       localStorage.setItem("articulos", JSON.stringify(articulos_lista));
+      
+$("#title").val("");
+$("#URL").val("");
+$("#price").val("");
+ $("#description").val("");
+   
+
+    // $(".alert").alert('close')
+var alerta = ['<div class="alert alert-success alert-dismissible fade show" role="alert">',
+                  '<strong>Se ha guardado correctamente el articulo!</strong>',
+                  '<button type="button" class="close" data-dismiss="alert" aria-label="Close">',
+                  '<span aria-hidden="true">&times;</span>',
+                  '</button>',
+                  '</div>'].join('\n');
+      $('#div_mensajes').append(alerta);
+ }
